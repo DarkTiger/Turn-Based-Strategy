979b04ab1e9f37fb5f2db13223a7b3ddf50eff38
DarkTiger <darktigerdevelop@gmail.com>
2017-07-11 17:32:08 +0200

    First Steps (Blutti)

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class GridGenerator : MonoBehaviour
+{
+    public int worldSizeX = 0;
+    public int worldSizeY = 0;
+    GameObject tile;
+
+
+    void Start()
+    {
+        tile = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/TestTile.prefab", typeof(GameObject));
+
+        Generator();
+    }
+
+
+    void Generator()
+    {
+        for (int y = 0; y < worldSizeY; y++)
+        {
+            for (int x = 0; x < worldSizeX; x++)
+            {
+                if ((y % 2) != 0)
+                {
+                    Instantiate(tile, new Vector3(x, y-0.670f, 1), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(tile, new Vector3(x+0.5f, y-0.670f, 1), Quaternion.identity);
+                } 
+            }
+        }
+    }
+}
