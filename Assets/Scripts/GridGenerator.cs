c491268227047a12832ed5920cccbeb05fab1f52
DarkTiger <darktigerdevelop@gmail.com>
2017-07-12 11:08:59 +0200

    Added Units Test and 'R' to Regenerate World

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 
 public class GridGenerator : MonoBehaviour
@@ -10,17 +11,24 @@ public class GridGenerator : MonoBehaviour
     public int worldSizeY = 0;
     GameObject tileType1; //green
     GameObject tileType2; //yellow
+    List<GameObject> worldTiles;
+    GameObject pg;
+
 
 
     void Start()
     {
         tileType1 = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/TestTile.prefab", typeof(GameObject));
         tileType2 = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/TestTileYellow.prefab", typeof(GameObject));
-        Generator();
+        pg = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Pg.prefab", typeof(GameObject));
+        worldTiles = new List<GameObject>();
+
+        WorldGenerator();
+        AddSomeUnits();
     }
 
 
-    void Generator()
+    void WorldGenerator()
     {
         for (int y = 0; y < worldSizeY; y++)
         {
@@ -34,10 +42,38 @@ public class GridGenerator : MonoBehaviour
                     tileTemp = tileType2;
                 }
 
-                GameObject newTile = Instantiate(tileTemp, new Vector3(xTemp, y / 1.5f, 1), Quaternion.identity);
+                GameObject newTile = Instantiate(tileTemp, new Vector3(xTemp, y / 1.5f, 1), tileTemp.transform.rotation);
                 Color tileColor = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), 1);
                 newTile.GetComponent<SpriteRenderer>().color = tileColor;
+
+                worldTiles.Add(newTile);
             }
+        }
+    }
+
+
+    void AddSomeUnits()
+    {   
+        foreach (GameObject tile in worldTiles)
+        {
+            Vector3 newPos = tile.transform.position;
+            newPos.y += 0.3f;
+            newPos.z = -1;
+
+            if (Random.Range(1, 10) == 4)
+            {
+                GameObject newPG = Instantiate(pg, newPos, Quaternion.identity);
+                newPG.transform.Rotate(new Vector3(0, 0, 7));
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            EditorSceneManager.LoadScene("Main");
         }
     }
 }
