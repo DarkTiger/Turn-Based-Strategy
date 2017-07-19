c9554f1fa728e5a27c2d911710977c65a56fcf5c
DarkTiger <darktigerdevelop@gmail.com>
2017-07-19 12:32:09 +0200

    Yeeeeeee

---
+++ Assets/Scripts/UnitStatsTextScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitStatsTextScript : MonoBehaviour
+{
+    Text attackText;
+    Text defenseText;
+    UnitScript unitScript;
+
+
+    void Start()
+    {
+        unitScript = GetComponent<UnitScript>();
+        attackText = transform.GetChild(0).GetComponent<Text>();
+        defenseText = transform.GetChild(1).GetComponent<Text>();
+    }
+
+
+    void Update()
+    {
+        int bonusAttack = unitScript.bonusAttack;
+        int baseDamage = unitScript.stats.damage;
+        int bonusDefense = unitScript.bonusDefense;
+        int baseHealth = unitScript.stats.health;
+
+        attackText.text = bonusAttack + "\n" + baseDamage;
+        defenseText.text = bonusDefense + "\n" + baseHealth;
+    }
+}
