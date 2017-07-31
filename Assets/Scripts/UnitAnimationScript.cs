f5556ba6651001a9b5950d69a67df5373f7e3214
DarkTiger <darktigerdevelop@gmail.com>
2017-07-31 14:57:32 +0200

    First  Animation Implementation

--- Assets/Scripts/UnitAnimationScript.cs
+++ Assets/Scripts/UnitAnimationScript.cs
@@ -5,36 +5,78 @@ using UnityEngine;
 public class UnitAnimationScript : MonoBehaviour 
 {
     public Animator animator;
+    int attackerRoleIndex;
 
 
 
 	void Start () 
     {
-	    animator = transform.GetChild(6).gameObject.GetComponent<Animator>();	
-	}
+	    animator = transform.GetChild(6).gameObject.GetComponent<Animator>();
+        attackerRoleIndex = GetComponent<UnitScript>().roleIndex;
+    }
 	
 
 	void Update()
     {
         if (Input.GetKeyUp(KeyCode.A))
         {
-            animator.SetBool("Attack", true);
+            PlayHitAnimation(2, false);
         }
 
         if (Input.GetKeyUp(KeyCode.S))
         {
-            animator.SetBool("Ability", true);
+            PlayHitAnimation(2, true);
         }
 
-        //Animation anim = animator.an animation;
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            PlayHitAnimation(3, false);
+        }
 
+
+        CheckAnimationsState();
+    }
+
+
+    /// <summary>
+    /// PLAYA L'ANIMAZIONE DELL'ATTACCO
+    /// </summary>
+    /// <param name="attackerRoleIndex">INDICE CLASSE ATTACCANTE</param>
+    /// <param name="isAbility">If set to <c>true</c> E' UN'ABILITA' (true, false)</param>
+    public void PlayHitAnimation(int attackerRoleIndex, bool isAbility)
+    {
+        animator.SetInteger("AttackerRoleIndex", attackerRoleIndex);
+
+        if (isAbility)
+        {
+            animator.SetBool("Ability", true);
+            /*switch (attackerRoleIndex)
+            {
+                case 0: animator.SetBool("Ability", true); break;
+                case 1: animator.SetBool("Ability", true); break;
+                case 2: animator.SetBool("Ability", true); break;
+                case 3: animator.SetBool("Ability", true); break;
+                case 4: animator.SetBool("Ability", true); break;
+                case 5: animator.SetBool("Ability", true); break;
+            }*/
+        }
+        else
+        {
+            animator.SetBool("Attack", true);
+        }
+    }
+
+
+    void CheckAnimationsState()
+    {
         AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
 
-        if (state.IsName("PorcupineAttack"))
+        if (state.IsName("PorcupineAttack") || state.IsName("RabbitAttack"))
         {
             if (state.normalizedTime >= state.length)
             {
                 animator.SetBool("Attack", false);
+                animator.SetInteger("AttackerRoleIndex", -1);
             }
         }
         else if (state.IsName("PorcupineAbility"))
@@ -42,16 +84,8 @@ public class UnitAnimationScript : MonoBehaviour
             if (state.normalizedTime >= state.length)
             {
                 animator.SetBool("Ability", false);
+                animator.SetInteger("AttackerRoleIndex", -1);
             }
         }
-
-
-        Debug.Log(state.IsName("PorcupineAbility").ToString() + "  " + state.normalizedTime.ToString() + "  " + state.length.ToString());
-
-        /*if (!anim.isPlaying)
-        {
-            animator.SetBool("Attack", false);
-        }*/
     }
-
 }
